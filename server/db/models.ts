7c16794ea754aabed65deab523b9581b3fc141e7
Subhojeet Ghosh <subhojeet.jis@gmail.com>
2025-04-25 01:00:35 +0530

    Update models.ts

--- server/db/models.ts
+++ server/db/models.ts
@@ -36,6 +36,11 @@ const issueSchema = new Schema({
     enum: ['bug', 'feature', 'documentation', 'security', 'performance'],
     default: 'bug'
   },
+  team: {
+    type: String,
+    enum: ['compass', 'meta', 'everest'],
+    default: 'compass'
+  },
   assigneeId: { type: Number, default: null },
   reporterId: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
