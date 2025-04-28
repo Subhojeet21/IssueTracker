fe872c2fe5611d23b6df06929a583e35a430640e
subhojeet.jis <subhojeet.jis@gmail.com>
2025-04-28 04:48:19 +0000

    word warp on issue desc

--- client/src/components/issues/IssueDetail.tsx
+++ client/src/components/issues/IssueDetail.tsx
@@ -146,7 +146,7 @@ const IssueDetail = ({
               
               <div className="mt-4">
                 <h3 className="text-sm font-medium text-gray-700 mb-2">Description</h3>
-                <div className="text-gray-700 whitespace-pre-line">
+                <div className="text-gray-700 whitespace-pre-wrap" style={{ wordWrap: 'break-word', overflowWrap: 'break-word' }}>
                   {issue.description}
                 </div>
               </div>
